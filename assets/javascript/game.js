17cf3d445d42aace72f1d9ac3b8abfd028b7fd9a
BrielleCarvalho <brielle.127@gmail.com>
2019-01-07 17:48:12 -0600

    still trying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,11 +23,14 @@
     var losses = 0;
     var numberGuesses = 12;
     var lettersUsed = [];
-    var letterReplaced;
+    
+    // var replaceLetter;
 
     // Randomly choosing a word
     var wordChoice = words[Math.floor(Math.random() * words.length)];
     console.log(wordChoice);
+
+
 
     //Putting blank spots where the letters will go.
     var answerArray = [];
@@ -38,9 +41,12 @@
     }
     guess.textContent = "Word to guess: " + answerArray.join(' ');
   
-  
+   
+
     // Computer recognizing keys pressed
     document.onkeyup = function(e){
+
+        
         
         // Setting users choice to the key pressed.
         lettersUsed.push(" " + e.key);
@@ -52,19 +58,89 @@
         console.log(numberGuesses);
         numberGuesses--;
         console.log(numberGuesses);   
+    
+    
+       
 
-        //turn event of key into lowercase
-        key = String.fromCharCode(event.keyCode).toLowerCase();
+        numberGuessesText.textContent = "Numer of guesses left: " + numberGuesses;
+        guessesText.textContent = "Letters already used: " + lettersUsed;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
 
-        // Resetting number of guesses and changing the word to guess and adding losses.
-        if (numberGuesses < 1){
+
+        // //Trying to get it to recognize letter choice and replace underscores with answers.
+        // if (wordChoice == "ecstatic"){
+        //     if (e.key === "e") {
+        //         answerArray = ["_", "_", "_", "_", "_", "_", "_", "_"];
+        //         answerArray.splice (0, 1, "e");
+        //         console.log(answerArray);
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+        //     if (e.key === "c") {
+        //         answerArray = ["_", "_", "_", "_", "_", "_", "_", "_"];
+        //         answerArray.splice(1, 1, "c");
+        //         answerArray.splice(7, 1, "c");
+        //         console.log(answerArray);
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+        //     if (e.key === "s") {
+        //         answerArray = ["_", "_", "_", "_", "_", "_", "_", "_"];
+        //         answerArray.splice(2, 1, "s");
+        //         console.log(answerArray);
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+        //     if (e.key === "t") {
+        //         answerArray = ["_", "_", "_", "_", "_", "_", "_", "_"];
+        //         answerArray.splice(3, 1, "t");
+        //         answerArray.splice(5, 1, "t");
+        //         console.log(answerArray);
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+        //     if (e.key === "a") {
+        //         answerArray = ["_", "_", "_", "_", "_", "_", "_", "_"];
+        //         answerArray.splice(4, 1, "a");
+        //         console.log(answerArray);
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+        //     if (e.key === "i") {
+        //         answerArray = ["_", "_", "_", "_", "_", "_", "_", "_"];
+        //         answerArray.splice(6, 1, "i");
+        //         console.log(answerArray);
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+
+        //     else {
+        //         var answerArray = [];
+        //         for (var i = 0; i < wordChoice.length; i++) {
+        //             answerArray.push(" _");
+        //             console.log(wordChoice[i]);
             
+        //         }
+        //         guess.textContent = "Word to guess: " + answerArray.join(' ');
+        //     }
+        // }
+
+    
+
+
+
+
+
+
+          // Resetting number of guesses and changing the word to guess and adding losses.
+          if (numberGuesses < 1){
+            
+            // If number of guesses runs out, add a "loss"
             losses ++;
             
+
+            // Refresh number of guesses
             numberGuesses = 12
 
+            // Refresh what letters have been used
             lettersUsed = [""];
 
+            // Select a new word
             var wordChoice = words[Math.floor(Math.random() * words.length)];
             console.log(wordChoice);
        
@@ -72,13 +148,8 @@
             for (var i = 0; i < wordChoice.length; i++) {
                 answerArray.push(" _");
                 console.log(wordChoice[i]);
-
-            //    if (wordChoice[i] === e.key) {
-            //        answerArray = answerArray.push(e.key);
-            //     guess.textContent = "Word to guess: " + answerArray;                  
-            //     }
-                
             }
+            
             guess.textContent = "Word to guess: " + answerArray.join(' ');
 
         }
@@ -87,8 +158,12 @@
         guessesText.textContent = "Letters already used: " + lettersUsed;
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
-     
+
     }
+
+        
+    
+    
 
     numberGuessesText.textContent = "Numer of guesses left: " + numberGuesses;
     guessesText.textContent = "Letters already used: " + lettersUsed;
@@ -99,11 +174,9 @@
 
 
         //check for letters on chosen word
-        
         //make letters appear in place of underscores
         //when word is finished, add a "win"
-        //if number of guesses runs out, add a "loss"
-        //select a new word
-        //refresh number of guesses
+        
+        
 
     
